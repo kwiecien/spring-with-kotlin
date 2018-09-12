37e92e27ac3f6cfd7912a0ca9d77fecef420bbd7
kwiecien <k.d.kwiecinski@gmail.com>
2018-09-12 22:23:54 +0200

    feat: Provide a custom Mustache.Compiler bean to be able to render HTML

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -1,10 +1,18 @@
 package com.example.blog
 
+import com.samskivert.mustache.Mustache
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class BlogApplication
+class BlogApplication {
+
+    @Bean
+    fun mustacheCompiler(loader: Mustache.TemplateLoader?) =
+            Mustache.compiler().escapeHTML(false).withLoader(loader)
+
+}
 
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args)
